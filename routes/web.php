919fed7a321d42bbd4029472164d96e3bbda8b6a
francescomascellino <franesco.mascellino@gmail.com>
2023-10-25 19:26:31 +0200

    fix typo

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/lorem', function () {
 });
 
 Route::get('/about', function () {
-    $title = 'This is only anoter lorem';
+    $title = 'This is only another lorem';
     $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem officiis necessitatibus atque repudiandae delectus dicta earum ratione aut sed similique nihil illum quos laborum, aliquam quo repellendus numquam iure. Saepe.';
     return view('about', compact('title', 'text'));
 });
